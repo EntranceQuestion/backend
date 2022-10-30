390c5b2bb473f33b5c6feea254e47599842194f5
Ali Shahid <shaeinst@gmail.com>
2022-10-31 00:29:34 +0545

    fix

--- backend_entrancequestion/settings.py
+++ backend_entrancequestion/settings.py
@@ -27,16 +27,6 @@ ALLOWED_HOSTS = [
 #     "http://localhost:8080",
 #     "http://127.0.0.1:9000",
 # ]
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "https://entrancequestion.com",
-    "https://apiofentrancequestion.entrancequestion.com",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://entrancequestion.com",
-    "https://apiofentrancequestion.entrancequestion.com",
-]
 
 # Application definition
 
@@ -139,3 +129,6 @@ STATIC_ROOT = "/home/entranc1/public_html/api_entrancequestion/static"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+
