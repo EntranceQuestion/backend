6654c7731a69c2320dd3fb25c2e993887ed6184b
Ali Shahid <shaeinst@gmail.com>
2022-11-01 23:52:40 +0545

    fix

--- api/admin.py
+++ api/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.ModelQuestion)
+
+# change headlines
+admin.site.site_header = "EntranceQuestion.com admin portal"
+admin.site.site_title = "EntranceQuestion"
+admin.site.index_title = "Administration"
