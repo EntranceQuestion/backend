716842218fbb11066dce5a53289aff39c622ec9d
Ali Shahid <shaeinst@gmail.com>
2022-11-07 00:22:28 +0545

    add true random support by caching solved questions ids

--- api/views.py
+++ api/views.py
@@ -2,12 +2,16 @@ import random
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import ModelQuestion
-from .serializers import ModelQuestionSerializer
+
+# from .serializers import ModelQuestionSerializer
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def model_question(request):
-    question_ids = list( ModelQuestion.objects.order_by("id").values_list("id", flat=True))
+    total_question_ids = list(
+        ModelQuestion.objects.order_by("id").values_list("id", flat=True)
+    )
+    question_ids = total_question_ids.copy()
 
     # number of model questions to return per request
     no_of_model_question = 50
@@ -15,11 +19,27 @@ def model_question(request):
     if len_question_ids < no_of_model_question:
         no_of_model_question = len_question_ids
 
+    len_solved_ids = len(request.data)
+
+    solved_question_ids = []
+    if len_solved_ids > 0:
+        solved_question_ids = list(request.data)[0].strip("[]").split(",")
+        # removed all solved question ids
+        for id in solved_question_ids:
+            id = int(id.strip('"'))
+            question_ids.remove(id)
+
+    if len(question_ids) < no_of_model_question:
+        no_of_model_question = len(total_question_ids)
+        question_ids = total_question_ids
+        solved_question_ids = []
+
     random_ids = random.sample(question_ids, no_of_model_question)
-    # print("----------- random ids ----------------")
-    # print(random_ids)
-    # print("--------------------------------------")
     model_questions = []
+    to_save_ids = random_ids + solved_question_ids
+    model_questions.append(
+        to_save_ids
+    )  # keep record of solved ids in user's local storage
     for id in random_ids:
         question_data = ModelQuestion.objects.get(pk=id)
 
@@ -30,7 +50,7 @@ def model_question(request):
             "option_2": question_data.option_2,
             "option_3": question_data.option_3,
             "option_4": question_data.option_4,
-            "correct_option": question_data.correct_option
+            "correct_option": question_data.correct_option,
         }
         model_questions.append(result)
 
